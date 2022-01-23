d67a544159df4e6957eebb22834d95ac89b8de39
Tejaswini Koduri <koduri.tejaswini58@gmail.com>
2022-01-23 10:50:19 -0500

    added forecast logic

--- src/app.js
+++ src/app.js
@@ -21,6 +21,14 @@ function formatDate(timestamp) {
   return `${days[day]} ${hours}:${minutes}`;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "f1e5044fce63dedfebf7bf6f1c98f16b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let tempElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -41,6 +49,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIconElement.setAttribute("alt", response.data.weather[0].description);
+  let coordinates = response.data.coord;
+  getForecast(coordinates);
 }
 
 function search(cityName) {
@@ -72,6 +82,42 @@ function showCelciusTemp(event) {
   temperatureElement.innerHTML = celciusTemperature;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weather-forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastday, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+                  <div class="weather-forecast-day">${formatDay(
+                    forecastday.dt
+                  )}</div>
+                  <img src="http://openweathermap.org/img/wn/${
+                    forecastday.weather[0].icon
+                  }@2x.png" alt="" width="36"/>
+                  <div class="weather-forecast-temp">
+                     <span class="weather-forecast-temp-max">
+                      ${Math.round(forecastday.temp.max)}°
+                     </span>
+                     <span class="weather-forecast-temp-min">
+                     ${Math.round(forecastday.temp.min)}°
+                    </span>
+                  </div>
+        </div> 
+          `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 let celciusTemperature = null;
 search("Alpharetta");
 
